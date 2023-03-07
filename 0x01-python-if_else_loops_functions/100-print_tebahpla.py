800ddf3371605dd732e9c0a232b8fbe8db3beae1
adelbertmatara <adelbertmatara@gmail.com>
2023-03-07 11:22:30 -0800

    fixed

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(90, 64, -1):
-    if i % 2 is 0:
-        i += 32
-    print("{}".format(chr(i)), end='')
-
+for i in range(26, 0, -1):
+    offset = 96 if i % 2 == 0 else 64
+    print("{}".format(chr(offset+i)), end="")
